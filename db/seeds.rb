780b58e613b7726db51ea2518de08436d8d58f07
Katharina Egler <katharina.egler@gmail.com>
2019-08-19 19:07:16 -0400

    changes in bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Chicken.create(name: "Hugo")
+#Chicken.create(name: "Kia", age: 2, egg_color: "blue", price: 50, egg_volume: 2, description: "She is a very calme and family-friendly chicken" )
+#Booking.create(start_date: "24.8.2019", end_date: "12.8.2020", user_id: 1, chicken_id: 1)
+#Booking.create(start_date: "13.8.2020", end_date: "20.8.2020", user_id: 2, chicken_id: 1)
+#User.create(name: "John Smith", location: "Montreal", rating: 5)
