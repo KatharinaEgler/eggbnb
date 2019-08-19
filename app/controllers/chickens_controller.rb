969c27905ab094d257de714c3f0229812955fc49
Lea Pontet <lea.pontet@hotmail.fr>
2019-08-19 18:11:42 -0400

    rename view files

--- app/controllers/chickens_controller.rb
+++ app/controllers/chickens_controller.rb
@@ -1,2 +1,26 @@
 class ChickensController < ApplicationController
+  def index
+    @chickens = Chicken.all
+  end
+
+  def show
+    @chicken = Chicken.find(params[:id])
+  end
+
+  def new
+    @chicken = Chicken.new
+  end
+
+  def create
+    @chicken = Chicken.new(c)
+  end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  # def destroy
+  # end
 end
