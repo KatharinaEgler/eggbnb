c9761540e98f34013d55024c62c8af6e66d009d2
Katharina Egler <katharina.egler@gmail.com>
2019-08-19 16:19:34 -0400

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chickens, only: [:create, :new, :index, :show] do
-    resources :bookings, only: [:new, :show]
+    resources :bookings, only: [:create]
   end
+
+  resources :bookings, only: [:index, :update]
 
   get "chickens/dashboard", to: "chickens#dashboard", as: "dashboard"
 
