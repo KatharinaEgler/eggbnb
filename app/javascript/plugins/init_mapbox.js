83764e738a4c944974d29f693a550b200aba15a8
courtney handy <courtney.e.handy@gmail.com>
2019-08-21 15:49:09 -0400

    updating map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,8 +21,8 @@ const initMapbox = () => {
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
       element.style.backgroundSize = 'contain';
-      element.style.width = '80px';
-      element.style.height = '80px';
+      element.style.width = '30px';
+      element.style.height = '54px';
 
       new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
