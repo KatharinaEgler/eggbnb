3452ad01c19943da1e58648d7def29f90c145b81
Katharina Egler <katharina.egler@gmail.com>
2019-08-22 15:50:56 -0400

    changes in profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :articles
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :chickens, only: [:create, :new, :index, :show] do
-    resources :bookings, only: [:create]
+  resources :chickens, only: [:create, :new, :index, :show, :destroy] do
+    resources :bookings, only: [:create, :destroy]
   end
 
   resources :bookings, only: [:index, :update, :show]
+  resources :users, only: [:index, :update, :show, :destroy]
 
-  get "chickens/dashboard", to: "chickens#dashboard", as: "dashboard"
+  # get "chickens/dashboard", to: "chickens#dashboard", as: "dashboard"
 
 end
