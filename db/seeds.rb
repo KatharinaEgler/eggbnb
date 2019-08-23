da4277fd778db75c9fffbfe08a898eb5b9d7e649
Katharina Egler <katharina.egler@gmail.com>
2019-08-23 11:23:11 -0400

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,45 @@ User.destroy_all
 Chicken.destroy_all
 Booking.destroy_all
 
-john = User.new(first_name: "John", last_name: "Smith", location: "Montreal", rating: 5, email: "john@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414756/caroline-hernandez-TMpQ5R9mbOc-unsplash_xowaqt.jpg")
-john.save!
-bill = User.new(first_name: "Bill", last_name: "Farmer", location: "Montreal", rating: 5, email: "bill@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414586/gregory-hayes-QFmNQXLPbZc-unsplash_uzib8w.jpg")
-bill.save!
-nora = User.new(first_name: "Nora", last_name: "Fischer", location: "Montreal", rating: 4, email: "nora@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566514738/sage-kirk-Wx2AjoLtpcU-unsplash_u74amp.jpg")
+joana = User.new(first_name: "Joana", last_name: "Smith", location: "Montreal", rating: 5, email: "joana@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572094/courtney_cmqmbb.jpg")
+joana.save!
+maria = User.new(first_name: "Maria", last_name: "Farmer", location: "Montreal", rating: 5, email: "maria@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572091/lea_cyeubu.jpg")
+maria.save!
+nora = User.new(first_name: "Nora", last_name: "Fischer", location: "Montreal", rating: 4, email: "nora@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572097/katharina_ykkv5e.jpg")
 nora.save!
+louis = User.new(first_name: "Louis", last_name: "Miller", location: "Montreal", rating: 4, email: "louis@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572103/louis_ku60k5.jpg")
+louis.save!
+samuel = User.new(first_name: "Samuel", last_name: "King", location: "Montreal", rating: 4, email: "samuel@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572110/sam_oewkes.jpg")
+samuel.save!
+chen = User.new(first_name: "Chen", last_name: "City", location: "Montreal", rating: 4, email: "chen@email.com", password: "chicken", remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566572113/chen_jlke6m.jpg")
+chen.save!
 
 #Chicken.create!(name: "Hugo")
 
-kia = Chicken.new(name: "Kia", address:"1950 Rue Gilford, Montreal", age: 2, egg_color: "blue", price: 0.5, egg_volume: 2, description: "She is a very calme and family-friendly chicken", user_id: john.id,remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414587/sven-hornburg-Jgabeh9Yx6w-unsplash_x24lqt.jpg")
+kia = Chicken.new(name: "Kia", address:"1950 Rue Gilford, Montreal", age: 2, egg_color: "blue", price: 0.5, egg_volume: 2, description: "She is a very calme and family-friendly chicken", user_id: maria.id,remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414587/sven-hornburg-Jgabeh9Yx6w-unsplash_x24lqt.jpg")
 kia.save!
-roberta = Chicken.new(name: "Roberta", address:"3564 Rue Saint-Denis, Montreal", age: 3, egg_color: "green", price: 0.4, egg_volume: 1, description: "She is a relaxed chicken", user_id: bill.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414586/john-towner-0uN9iF4mgDI-unsplash_bqyg2m.jpg")
+roberta = Chicken.new(name: "Roberta", address:"3564 Rue Saint-Denis, Montreal", age: 3, egg_color: "green", price: 0.4, egg_volume: 1, description: "She is a relaxed chicken", user_id: maria.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414586/john-towner-0uN9iF4mgDI-unsplash_bqyg2m.jpg")
 roberta.save!
-pia = Chicken.new(name: "Pia", address:"5333 Av. Casgrain, Montreal", age: 3, egg_color: "brown", price: 0.4, egg_volume: 1, description: "She is a bit loud", user_id: bill.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414586/shannon-vandenheuvel-bI9AeUn15_A-unsplash_cbeeca.jpg")
+pia = Chicken.new(name: "Pia", address:"5333 Av. Casgrain, Montreal", age: 3, egg_color: "brown", price: 0.4, egg_volume: 1, description: "She is a bit loud", user_id: maria.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566414586/shannon-vandenheuvel-bI9AeUn15_A-unsplash_cbeeca.jpg")
 pia.save!
-hugo = Chicken.new(name: "Hugo", address:"1251 Rue Gilford, Montréal", age: 1, egg_color: "white", price: 0.3, egg_volume: 1, description: "She is very good with animals", user_id: john.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566419185/vidar-nordli-mathisen-dC-z4r8tr6U-unsplash_mjoojj.jpg")
+hugo = Chicken.new(name: "Hugo", address:"1251 Rue Gilford, Montréal", age: 1, egg_color: "white", price: 0.3, egg_volume: 1, description: "She is very good with animals", user_id: samuel.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566419185/vidar-nordli-mathisen-dC-z4r8tr6U-unsplash_mjoojj.jpg")
 hugo.save!
-first_booking = Booking.new(start_date: Date.parse("2019-09-07"), end_date: Date.parse("2019-10-23"), user_id: john.id, chicken_id: kia.id)
+
+bossy = Chicken.new(name: "Bossy", address:"4581 Park Ave, Montreal", age: 5, egg_color: "pink", price: 0.6, egg_volume: 4, description: "She chases everyone around, but is very good with other chicken. She is our chicken boss.", user_id: maria.id,remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566573112/ehud-neuhaus-LJOTTEf6DaQ-unsplash_p8zly6.jpg")
+bossy.save!
+meggy = Chicken.new(name: "Meggy", address:"2047 Mont-Royal Ave E, Montreal", age: 3, egg_color: "green", price: 0.4, egg_volume: 1, description: "She does not like people and is very shy", user_id: maria.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566573112/kazi-faiz-ahmed-jeem-hXXncjTJJ1g-unsplash_wpllmn.jpg")
+meggy.save!
+dani = Chicken.new(name: "Dani", address:"1019 Mont-Royal Ave E", age: 3, egg_color: "brown", price: 0.4, egg_volume: 1, description: "She is a bit crazy", user_id: maria.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566573114/jason-leung-AVbFNg1r9fg-unsplash_arsvty.jpg")
+dani.save!
+sandy = Chicken.new(name: "Sandy", address:"3801 Rue Université, Montréal", age: 10, egg_color: "white", price: 0.3, egg_volume: 1, description: "She loves dogs", user_id: samuel.id, remote_photo_url: "https://res.cloudinary.com/dd9qabk1u/image/upload/v1566573111/patrick-kariuki-AS5BN2vntRg-unsplash_ohmwwy.jpg")
+sandy.save!
+
+
+first_booking = Booking.new(start_date: Date.parse("2019-09-07"), end_date: Date.parse("2019-10-23"), user_id: maria.id, chicken_id: kia.id)
 first_booking.save!
-second_booking = Booking.new(start_date: Date.parse("2019-08-23"), end_date: Date.parse("2019-08-30"), user_id: john.id, chicken_id: roberta.id)
+second_booking = Booking.new(start_date: Date.parse("2019-08-23"), end_date: Date.parse("2019-08-30"), user_id: maria.id, chicken_id: roberta.id)
 second_booking.save!
-third_booking = Booking.new(start_date: Date.parse("2019-08-23"), end_date: Date.parse("2019-08-30"), user_id: john.id, chicken_id: hugo.id)
+third_booking = Booking.new(start_date: Date.parse("2019-08-23"), end_date: Date.parse("2019-08-30"), user_id: maria.id, chicken_id: hugo.id)
 third_booking.save!
 #Booking.create(start_date: "13.8.2020", end_date: "20.8.2020", user_id: 2, chicken_id: 1)
 puts "seed completed"
